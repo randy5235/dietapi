b842ec59d79a4cdbe0583085c116403ae064a6eb
Randy Wressell <rwressell@globesherpa.com>
2017-02-15 20:33:25 -0800

    Add routes

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Randy Wressell on 2/15/17.
+ */
+
+'use strict'
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const routes = require('./routes/route');
