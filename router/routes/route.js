b674796c2b7dddad57cc97c7c2edd2025a2de9c3
Randy Wressell <randy5235@hotmail.com>
2017-04-23 20:54:16 -0700

    Add linting and fix linting errors

--- router/routes/route.js
+++ router/routes/route.js
@@ -1,57 +1,52 @@
 const express = require('express');
-let dataModel = require('../../lib/dataModel');
-let id = (req) => {return req.params.id}
+const dataModel = require('../../lib/dataModel');
 
+const id = req => req.params.id;
 
 // Routes
 const router = express.Router();
-router.get('/:id', (req,res) => {
-    // let id = req.params.id;
-    // let ret = {};(
-    // ret[id(req)] = dataModel.model[id];
-    if (dataModel.model[id(req)]){
-        res.send(dataModel.model[id(req)]);
-    }
-    else {
-        res.status(400).send(`${id(req)} does not exist`);
-    }
+router.get('/:id', (req, res) => {
+  // let id = req.params.id;
+  // let ret = {};(
+  // ret[id(req)] = dataModel.model[id];
+  if (dataModel.model[id(req)]) {
+    res.send(dataModel.model[id(req)]);
+  } else {
+    res.status(400).send(`${id(req)} does not exist`);
+  }
 });
 
-router.post('/:id', (req,res) => {
-    // let id = req.params.id;
-    if (dataModel.model[id(req)]) {
-        res.status(400).send(`${id(req)} error: VALUE ALREADY EXISTS`);
-    }
-    else {
-        dataModel.model[id(req)] = req.body;
-        console.log(dataModel.model);
-        res.json({message: 'Huzzah it was added!'});
-    }
+router.post('/:id', (req, res) => {
+  // let id = req.params.id;
+  if (dataModel.model[id(req)]) {
+    res.status(400).send(`${id(req)} error: VALUE ALREADY EXISTS`);
+  } else {
+    dataModel.model[id(req)] = req.body;
+    console.log(dataModel.model);
+    res.json({ message: 'Huzzah it was added!' });
+  }
 });
 
 router.put('/:id', (req, res) => {
-    if (!dataModel.model[req.params.id]) {
-        res.status(400).send(`${req.params.id} does not exist!`);
-    }
-    else {
-        // let id = req.params.id;
-        dataModel.model[id(req)] = req.body;
-        let response = {};
+  if (!dataModel.model[req.params.id]) {
+    res.status(400).send(`${req.params.id} does not exist!`);
+  } else {
+    // let id = req.params.id;
+    dataModel.model[id(req)] = req.body;
+    const response = {};
 
-        console.log(dataModel.model);
-        res.send(dataModel.model[id(req)]);
-    }
+    console.log(dataModel.model);
+    res.send(dataModel.model[id(req)]);
+  }
 });
 
 module.exports = router;
 
 router.delete('/:id', (req, res) => {
-    if (!dataModel.model[req.params.id]) {
-        res.status(400).send(`${req.params.id} does not exist`);
-    }
-    else {
-        delete dataModel.model[id(req)];
-        res.status(400).send(`${req.params.id} has been deleted`);
-
-    }
+  if (!dataModel.model[req.params.id]) {
+    res.status(400).send(`${req.params.id} does not exist`);
+  } else {
+    delete dataModel.model[id(req)];
+    res.status(400).send(`${req.params.id} has been deleted`);
+  }
 });
--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-'use strict'
-
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const router = require('./router/routes/route');
 
-
+const app = express();
 
 app.use(bodyParser.json());
 
-let port = process.env.PORT || 8080
-
+const port = process.env.PORT || 8080;
 
 
 app.use('/api', router);
 
 
 app.listen(port);
-console.log(`Server is listening on ${port}`);
+// console.log(`Server is listening on ${port}`);
