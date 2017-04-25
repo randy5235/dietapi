387e506c82221878a971a3d944751cd8601ce4a6
Randy Wressell <randy5235@hotmail.com>
2017-04-24 21:47:50 -0700

    Consolidate route and add nodemon

--- router/routes/route.js
+++ router/routes/route.js
@@ -5,49 +5,48 @@ const id = req => req.params.id;
 
 // Routes
 const router = express.Router();
-router.get('/:id', (req, res) => {
-  // let id = req.params.id;
-  // let ret = {};(
-  // ret[id(req)] = dataModel.model[id];
-  if (dataModel.model[id(req)]) {
-    res.send(dataModel.model[id(req)]);
-  } else {
-    res.status(400).send(`${id(req)} does not exist`);
-  }
-});
-
-router.post('/:id', (req, res) => {
-  // let id = req.params.id;
-  if (dataModel.model[id(req)]) {
-    res.status(400).send(`${id(req)} error: VALUE ALREADY EXISTS`);
-  } else {
-    dataModel.model[id(req)] = req.body;
-    console.log(dataModel.model);
-    res.json({ message: 'Huzzah it was added!' });
-  }
-});
-
-router.put('/:id', (req, res) => {
-  if (!dataModel.model[req.params.id]) {
-    res.status(400).send(`${req.params.id} does not exist!`);
-  } else {
+router.route('/:id')
+  .get((req, res) => {
     // let id = req.params.id;
-    dataModel.model[id(req)] = req.body;
-    const response = {};
+    // let ret = {};(
+    // ret[id(req)] = dataModel.model[id];
+    if (dataModel.model[id(req)]) {
+      res.send(dataModel.model[id(req)]);
+    } else {
+      res.status(400).send(`${id(req)} does not exist`);
+    }
+  })
+  .post((req, res) => {
+    // let id = req.params.id;
+    if (dataModel.model[id(req)]) {
+      res.status(400).send(`${id(req)} error: VALUE ALREADY EXISTS`);
+    } else {
+      dataModel.model[id(req)] = req.body;
+      console.log(req.body);
+      console.log(dataModel.model);
+      res.json({ message: 'Huzzah it was added!' });
+    }
+  })
+  .put((req, res) => {
+    if (!dataModel.model[req.params.id]) {
+      res.status(400).send(`${req.params.id} does not exist!`);
+    } else {
+      // let id = req.params.id;
+      dataModel.model[id(req)] = req.body;
+      const response = {};
 
-    console.log(dataModel.model);
-    res.send(dataModel.model[id(req)]);
-  }
-});
-
-router.delete('/:id', (req, res) => {
-  if (!dataModel.model[req.params.id]) {
-    res.status(400).send(`${req.params.id} does not exist`);
-  } else {
-    delete dataModel.model[id(req)];
-    res.status(400).send(`${req.params.id} has been deleted`);
-  }
-});
+      console.log(dataModel.model);
+      res.send(dataModel.model[id(req)]);
+    }
+  })
+  .delete((req, res) => {
+    if (!dataModel.model[req.params.id]) {
+      res.status(400).send(`${req.params.id} does not exist`);
+    } else {
+      delete dataModel.model[id(req)];
+      res.status(400).send(`${req.params.id} has been deleted`);
+    }
+  });
 
 module.exports = router;
 
